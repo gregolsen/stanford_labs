baca04ce27adffc1ce1c64bb72fef37e51c247ec
gregolsen <anotheroneman@yahoo.com>
2011-11-27 12:11:04 +0200

    lab1 works - reads only from standard input

---
+++ 1/lab1.hs
@@ -0,0 +1,36 @@
+import qualified Data.Char as Char
+import qualified Data.Map as Map
+import qualified Data.List as List
+import qualified Control.Monad as Monad
+
+cutWord :: String -> String
+cutWord = reverse . dropWhile (`elem` "!.,:;-')(=") . reverse
+
+splitLine :: String -> [String]
+splitLine = filter (\x -> length x > 0) . map ((map Char.toLower) . cutWord) . words
+
+maxWordLen :: [String] -> Int
+maxWordLen = maximum . map length
+
+toDictList :: [String] -> [(String, Int)]
+toDictList = map (\x -> (x, 1))
+
+buildDict :: [(String, Int)] -> Map.Map String Int
+buildDict = Map.fromListWith (+)
+
+process :: [String] -> [(String, Int)]
+process = List.sortBy (\(_, v1) (_, v2) -> v2 `compare` v1) . Map.toList . buildDict . toDictList
+
+printWordBar :: Int -> (String, Int) -> IO ()
+printWordBar maxWord (word, value) =
+  let spaces = take (1 + maxWord - (length $ word)) $ cycle [' '] 
+      bar = take value $ cycle ['#']
+  in putStrLn $ word ++ spaces ++ bar
+
+main = do
+  text <- getContents
+  let 
+    words   = splitLine text
+    maxWord = maxWordLen words
+    result  = process words
+  mapM_ (printWordBar maxWord) result
